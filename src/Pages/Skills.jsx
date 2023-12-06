43a25ec86031021f25eca2042f4a1c6672c852f4
Marian Aphiare <140634802+marykevwe@users.noreply.github.com>
2023-12-06 11:20:05 -0500

    update pages

--- src/Pages/Skills.jsx
+++ src/Pages/Skills.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React from 'react';
 
 function Skills() {
   return (
-    <div>Skills</div>
-  )
+    <>
+    <div className='skills'>
+    <h2>Skills</h2>
+    <div className='flex'>
+
+      <div className="skill">
+        <img src="src/assets/react.svg" alt="React" />
+        <p>React</p>
+      </div>
+      <div className="skill">
+        <img src="src/assets/html2.png" alt="HTML" />
+        <p>HTML</p>
+      </div>
+      <div className="skill">
+        <img src="src/assets/JavaScript_logo_2.svg" alt="JavaScript" />
+        <p>JavaScript</p>
+      </div>
+      <div className="skill">
+        <img src="src/assets/Github.logo.png" alt="Redux" />
+        <p>Github</p>
+      </div>
+    </div>
+    </div>
+    </>
+  );
 }
 
-export default Skills
+export default Skills;
