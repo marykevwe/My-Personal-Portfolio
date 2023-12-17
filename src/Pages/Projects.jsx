5ac0264ef9923f5009ce5a1c12bad1084d385b7e
Marian Aphiare <140634802+marykevwe@users.noreply.github.com>
2023-12-17 07:08:16 -0500

    Update files

--- src/Pages/Projects.jsx
+++ src/Pages/Projects.jsx
@@ -6,7 +6,7 @@ function Projects() {
     <>
       <div className='flex'>
         <div className='container'>
-          <img src="src/assets/project1.png" alt="" style={{ maxWidth: '100%', maxHeight: '100%' }} />
+          <img src="/project1.png" alt="" style={{ maxWidth: '100%', maxHeight: '100%' }} />
           <p>Project 1</p>
           <a href="https://github.com/marykevwe/WEEK-12" target="_blank" rel="noopener noreferrer">
             GitHub Repository
@@ -14,7 +14,7 @@ function Projects() {
         </div>
 
         <div className='container'>
-          <img src="src/assets/ToDo.png" alt="" style={{ maxWidth: '100%', maxHeight: '100%' }} />
+          <img src="/ToDo.png" alt="" style={{ maxWidth: '100%', maxHeight: '100%' }} />
           <p>Project 2</p>
           <a href="https://github.com/marykevwe/WEEK-9" target="_blank" rel="noopener noreferrer">
             GitHub Repository
@@ -22,7 +22,7 @@ function Projects() {
         </div>
 
         <div className='container'>
-          <img src="src/assets/project3.png" alt="" style={{ maxWidth: '100%', maxHeight: '100%' }} />
+          <img src="/project3.png" alt="" style={{ maxWidth: '100%', maxHeight: '100%' }} />
           <p>Project 3</p>
           <a href="https://github.com/marykevwe/WEEK-14" target="_blank" rel="noopener noreferrer">
             GitHub Repository
@@ -30,7 +30,7 @@ function Projects() {
         </div>
 
         <div className='container'>
-          <img src="src/assets/project2.png" alt="" style={{ maxWidth: '100%', maxHeight: '100%' }} />
+          <img src="/project2.png" alt="" style={{ maxWidth: '100%', maxHeight: '100%' }} />
           <p>Project 4</p>
           <a href="https://github.com/marykevwe/JNLMS-Test" target="_blank" rel="noopener noreferrer">
             GitHub Repository
--- src/Pages/Skills.jsx
+++ src/Pages/Skills.jsx
@@ -9,19 +9,19 @@ function Skills() {
 
       <div className="skill">
         <div className='still-img1'>
-        <img src="src/assets/react.svg" alt="React" /></div>
+        <img src="/react.svg" alt="React" /></div>
         <p>React</p>
       </div>
       <div className="skill">
-      <div className='still-img2'> <img src="src/assets/html2.png" alt="HTML" /></div>
+      <div className='still-img2'> <img src="/html2.png" alt="HTML" /></div>
         <p>HTML</p>
       </div>
       <div className="skill">
-       <div className='still-img3'> <img src="src/assets/JavaScript_logo_2.svg" alt="JavaScript" /></div>
+       <div className='still-img3'> <img src="/JavaScript_logo_2.svg" alt="JavaScript" /></div>
         <p>JavaScript</p>
       </div>
       <div className="skill">
-      <div className='still-img4'> <img src="src/assets/Github.logo.png" alt="github" /></div>
+      <div className='still-img4'> <img src="/Github.logo.png" alt="github" /></div>
         <p>Github</p>
       </div>
     </div>
